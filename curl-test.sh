661c96117303995bad8dd19a9f1cd5ba6f7fdda9
lauren-ciha <64796985+Lauren-Ciha@users.noreply.github.com>
2022-06-22 16:15:22 -0500

    Add random name generator and test statement to curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -2,15 +2,29 @@
 
 # Tests GET and POST using Bash
 
-
 # create timeline post using POST
-output=$(curl -s -X POST http://localhost:5000/api/timeline_post -d 'name=random&email=notanemail@gmail.com&content=Testing POST')
+output=$(curl -s -X POST http://localhost:5000/api/timeline_post -d 'name=test&email=notanemail@gmail.com&content=Testing POST')
 
 # check GET to make sure it was added
 posts=$(curl -s http://localhost:5000/api/timeline_post)
 
-echo "$output"
-echo "$posts"
+# generate a random name
+name=""
+chars=abcd1234ABCD
+for i in {1..8} ; do
+    name="${chars:RANDOM%${#chars}:1}"
+done
+
+# echo "$output"
+if [[ $output == *"$name"* ]];
+then
+   echo "Post and get were successful"
+else
+    echo "Test failed"
+fi
+
+# variable=$(mysql -u myportfolio -p -D myportfoliodb -e "SELECT * from timelinepost WHERE name = '"\name\"';")
+# if [ -z $variable ]; then echo "Var is empty"; fi
 
 # Bonus: delete endpoint (not working at the moment, may come back to later)
 # delete=$(curl -s http://localhost:5000/api/timeline_post/drandom)
